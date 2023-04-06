cd379d0760717c4ec2aa033824c1c573d01c4316
Khalid <khalid@Khalids-Air.Home>
2023-04-06 13:24:36 +0530

    Create a view for home and profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,11 @@
-module.exports.profile = function(req,res){
-    return res.end('<h1> User Profile </h1>');
+module.exports.profile = function (req, res) {
+    // return res.end('<h1> User Profile </h1>');
+
+    return res.render('profile', {
+        title: "Profile"
+    });
 };
 
-module.exports.chats = function(req,res){
+module.exports.chats = function (req, res) {
     return res.end('<h1> User Chats</h1>');
 };
