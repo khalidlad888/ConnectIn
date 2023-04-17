b7b0737e7fe3b487c56a3d907f29c8abbf8364e4
Khalid <khalid@Khalids-Air.Home>
2023-04-17 14:20:23 +0530

    Create posts and show on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,44 @@
+const Post = require('../models/post');
+
+// module.exports.home = function (req, res) {
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 25);
+
+// Post.find({}, function (err, posts){
+//     return res.render('home', {
+//         title: "Faceloop | Home",
+//         posts: posts
+//     });
+//     });
+// };
+
+
 module.exports.home = function (req, res) {
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({})
+    //     .then(posts => {
+    //         return res.render('home', {
+    //             title: 'Home',
+    //             posts: posts
+    //         });
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //         return res.redirect('back');
+    //     });
+
+    Post.find({}).populate('user').exec()
+        .then(posts => {
+            return res.render('home', {
+                title: "Home",
+                posts: posts
+            });
+        })
+        .catch(err => {
+            console.error(err);
+            // handle error
+        });
+
 };
