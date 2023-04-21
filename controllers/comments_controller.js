c36a8387407116f98000489cf2989055afbc3c3d
Khalid <khalid@Khalids-Air.Home>
2023-04-21 16:20:54 +0530

    Deleting posts and comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -53,3 +53,52 @@ module.exports.create = function (req, res) {
             res.status(500).send('Error in adding comment');
         });
 };
+
+
+// module.exports.destroy = function(req, res){
+//     Comment.findById(req.params.id, function(err, comment){
+//         if (comment.user == req.user.id){
+//             let postId = comment.post;
+
+//             comment.deleteOne();
+//             Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}, function(err, post){
+//                 return redirect('back');
+//             }});
+//         }else{
+//             return res.redirect('back');
+//         };
+
+//     });
+// };
+
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id)
+    .then(comment => {
+        if (comment.user == req.user.id){
+            let postId = comment.post;
+
+            comment.deleteOne()
+            .then(() => {
+                return Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}})
+                .then(() => {
+                    return res.redirect('back');
+                })
+                .catch(err => {
+                    console.error(err);
+                    return res.redirect('back');
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        return res.redirect('back');
+    });
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 // module.exports.create = function(req, res){
 //     Post.create({
@@ -28,3 +29,40 @@ module.exports.create = function(req, res){
         return;
     });
 };
+
+
+// module.exports.destroy = function(req,res){
+//     Post.findById(req.params.id, function(err, post){
+//         if(post.user == req.user.id){
+//             post.remove();
+
+//             Comment.deleteMany({post: req.params.id}, function(err){
+//                 return res.redirect('back');
+//             });
+//         }else{
+//             return res.redirect('back');
+//         };
+//     });
+// }; 
+
+
+module.exports.destroy = function(req, res) {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (post.user == req.user.id) {
+          return post.deleteOne();
+        } else {
+          throw new Error("User is not authorized to delete this post");
+        }
+      })
+      .then(() => {
+        return Comment.deleteMany({ post: req.params.id });
+      })
+      .then(() => {
+        return res.redirect("back");
+      })
+      .catch(err => {
+        console.error(err);
+        return res.redirect("back");
+      });
+  };  
