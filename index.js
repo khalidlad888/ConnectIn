789a0f48987aa4702363c8a0922b8aa77a606c23
Khalid <khalid@Khalids-Air.Home>
2023-05-12 16:33:30 +0530

    create a chatroom for connectin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+const cors = require('cors');
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
@@ -13,6 +14,14 @@ const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+
+//setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000, ()=> {
+    console.log('chatServer is listening on port 5000');
+});
+
 
 
 app.use(sassMiddleware({
